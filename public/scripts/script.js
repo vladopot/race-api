aea7777ad9e22359fa5f59c1f739938869d59872
vladopot <vladsopot@mail.ru>
2023-07-21 01:06:05 +0300

    feat: add func

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,19 @@
-"use strict";
-const firstWords = "hello world";
-console.info(firstWords);
+var start_buttons = document.querySelectorAll(".start_engine_btn");
+start_buttons.forEach(function (e) {
+    var temp = e;
+    temp === null || temp === void 0 ? void 0 : temp.addEventListener("click", start_engine);
+});
+function start_engine() {
+    var targ = arguments[0].target;
+    var cars = document.querySelectorAll(".car_icon");
+    var car;
+    cars.forEach(function (e) {
+        var temp = e;
+        if (temp.dataset.carNum === targ.dataset.carNum) {
+            car = temp;
+            car.setAttribute("style", "transition-property: left; transition-duration: 9s;");
+            car.classList.add("race");
+            return;
+        }
+    });
+}
--- public/scripts/script.ts
+++ public/scripts/script.ts
@@ -1,6 +1,20 @@
-document.querySelector(".car")?.addEventListener("click", start_engine);
+const start_buttons = document.querySelectorAll(".start_engine_btn");
+start_buttons.forEach((e) => {
+    const temp: HTMLElement = <HTMLElement>e;
+    temp?.addEventListener("click", start_engine);
+});
 
 function start_engine() {
-    const targer = ;
-    const car = document.querySelector(".car_icon");
+    const targ = arguments[0].target;
+    const cars = document.querySelectorAll(".car_icon");
+    let car: HTMLElement | null;
+    cars.forEach((e) => {
+        const temp: HTMLElement = <HTMLElement>e;
+        if (temp.dataset.carNum === targ.dataset.carNum) {
+            car = <HTMLElement>temp;
+            car.setAttribute("style", "transition-property: left; transition-duration: 9s;");
+            car.classList.add("race");
+            return;
+        }
+    });
 }
