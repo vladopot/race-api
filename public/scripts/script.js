fb994c90b3839dbabeabe14572796982305737b6
vladsopot@mail.ru <vladsopot@mail.ru>
2023-07-22 15:17:24 +0300

    feat: new

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,41 @@
-var start_buttons = document.querySelectorAll(".start_engine_btn");
-start_buttons.forEach(function (e) {
-    var temp = e;
+"use strict";
+const start_buttons = document.querySelectorAll(".start_engine_btn");
+const race_btn = document.querySelector(".btn_race");
+const create_btn = document.querySelector(".create_btn");
+const requestURL = 'http://127.0.0.1:3000/garage';
+start_buttons.forEach((e) => {
+    const temp = e;
     temp === null || temp === void 0 ? void 0 : temp.addEventListener("click", start_engine);
 });
+race_btn === null || race_btn === void 0 ? void 0 : race_btn.addEventListener("click", race);
 function start_engine() {
-    var targ = arguments[0].target;
-    var cars = document.querySelectorAll(".car_icon");
-    var car;
-    cars.forEach(function (e) {
-        var temp = e;
+    const end_point = window.innerWidth / 75;
+    const targ = arguments[0].target;
+    const cars = document.querySelectorAll(".car_icon");
+    let car;
+    cars.forEach((e) => {
+        const temp = e;
         if (temp.dataset.carNum === targ.dataset.carNum) {
             car = temp;
-            car.setAttribute("style", "transition-property: left; transition-duration: 9s;");
-            car.classList.add("race");
+            car.setAttribute("style", `transform: translate(${75 * end_point - 95}px); transition-property: transform; transition-duration: 2s;`);
             return;
         }
     });
+}
+function race() {
+    const end_point = window.innerWidth / 75;
+    const cars = document.querySelectorAll(".car_icon");
+    cars.forEach((e) => {
+        const temp = e;
+        temp.setAttribute("style", `transform: translate(${75 * end_point - 95}px); transition-property: transform; transition-duration: 2s;`);
+    });
+}
+create_btn === null || create_btn === void 0 ? void 0 : create_btn.addEventListener("click", request);
+function request() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", requestURL);
+    xhr.responseType = "json";
+    let resp = xhr.response;
+    xhr.send();
+    console.log(resp);
 }
--- public/scripts/script.ts
+++ public/scripts/script.ts
@@ -1,10 +1,16 @@
 const start_buttons = document.querySelectorAll(".start_engine_btn");
+const race_btn = document.querySelector(".btn_race");
+const create_btn = document.querySelector(".create_btn");
+const requestURL:string = 'http://127.0.0.1:3000/garage';
+
 start_buttons.forEach((e) => {
     const temp: HTMLElement = <HTMLElement>e;
     temp?.addEventListener("click", start_engine);
 });
+race_btn?.addEventListener("click", race);
 
 function start_engine() {
+    const end_point: number = window.innerWidth / 75;
     const targ = arguments[0].target;
     const cars = document.querySelectorAll(".car_icon");
     let car: HTMLElement | null;
@@ -12,9 +18,25 @@ function start_engine() {
         const temp: HTMLElement = <HTMLElement>e;
         if (temp.dataset.carNum === targ.dataset.carNum) {
             car = <HTMLElement>temp;
-            car.setAttribute("style", "transition-property: left; transition-duration: 9s;");
-            car.classList.add("race");
+            car.setAttribute("style", `transform: translate(${75 * end_point - 95}px); transition-property: transform; transition-duration: 2s;`);
             return;
         }
     });
+}
+
+function race() {
+    const end_point: number = window.innerWidth / 75;
+    const cars = document.querySelectorAll(".car_icon");
+    cars.forEach((e) => {
+        const temp:HTMLElement = <HTMLElement>e;
+        temp.setAttribute("style", `transform: translate(${75 * end_point - 95}px); transition-property: transform; transition-duration: 2s;`);
+    });
+}
+
+create_btn?.addEventListener("click", request)
+const xhr = new XMLHttpRequest();
+function request() {
+    xhr.open("GET", requestURL);
+    console.log(JSON.parse(xhr.response));
+    xhr.send();
 }
